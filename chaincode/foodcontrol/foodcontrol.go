678a19253719b022061796664f5d0b35357cce4c
sirpask <el_pask@hotmail.com>
2020-10-02 14:16:23 +0200

    otra subida

--- chaincode/foodcontrol/foodcontrol.go
+++ chaincode/foodcontrol/foodcontrol.go
@@ -13,37 +13,43 @@ import (
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
 
-// SmartContract provides functions for control the food (definimos una estructura)
+// SmartContract provides functions for control the food (definimos una estructura ya predefinida)
 type SmartContract struct {
 	contractapi.Contract
 }
 
-//Food describes basic details of what makes up a food
+//Food describes basic details of what makes up a food (en la block chain guardaremos el tipo granjero y variedad de cultivo)
 type Food struct {
 	Farmer  string `json:"farmer"`
 	Variety string `json:"variety"`
 }
 
+//para crear una funcion que guarde en la blockchain es crear esa funcion s *Smartcontract es parte de la funcion y luego su novmbre Set (nombre ctx del tipo contractapi.transaction context, y luego añadimos las propiedades de los datos que se van a añadir a la blockchain)
 func (s *SmartContract) Set(ctx contractapi.TransactionContextInterface, foodId string, farmer string, variety string) error {
 
 	//Validaciones de sintaxis
 
-	//validaciones de negocio
+	//validaciones de negocio (estructura de go, esta estructura de binario hay que pasarla a json, y se guarde como conjunto de bytes, se hace con marshal)
 
 	food := Food{
 		Farmer:  farmer,
 		Variety: variety,
 	}
 
+	//se transforma en bytes para pasarlos a la blockchain o a json
 	foodAsBytes, err := json.Marshal(food)
 	if err != nil {
 		fmt.Printf("Marshal error: %s", err.Error())
 		return err
 	}
 
+	//getstub.putstate para permitir guardar en el leger (libro distribuido) como tal necesita un id y un valor) Putstate estampa de tiempo es el historial de tu activo,
+	//la estapa del tiempo es algo muy elemntal hace que guarde un estado en el libro distribuido, esta en cualquier blockchain
+	//
 	return ctx.GetStub().PutState(foodId, foodAsBytes)
 }
 
+//consultar en el ledger una transaccion. *Food hace referencia a la estructura , consultaremos el estado de una transaccion
 func (s *SmartContract) Query(ctx contractapi.TransactionContextInterface, foodId string) (*Food, error) {
 
 	foodAsBytes, err := ctx.GetStub().GetState(foodId)
@@ -74,7 +80,7 @@ func main() {
 		fmt.Printf("Error create foodcontrol chaincode: %s", err.Error())
 		return
 	}
-
+	//levantamos el codigo
 	if err := chaincode.Start(); err != nil {
 		fmt.Printf("Error starting foodcontrol chaincode: %s", err.Error())
 	}
